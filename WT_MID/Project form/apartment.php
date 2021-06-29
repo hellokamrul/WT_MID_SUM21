b351b15ac7f08a7c4d00f5eb2515cea70aefab3b
hellokamrul <nijumkamrul447@gmail.com>
2021-06-29 13:33:40 +0600

    project form complete

--- WT_MID/Project form/apartment.php
+++ WT_MID/Project form/apartment.php
@@ -2,6 +2,30 @@
     $title="";
     $title_error="";
     $error = false;
+
+    $type="";
+    $type_error ="";
+
+
+    $rate ="";
+    $rate_error="";
+
+
+    $broom="";
+    $room_error="";
+
+    $area="";
+    $area_error="";
+
+    $location="";
+    $location_error="";
+
+    $adress="";
+    $adress_error="";
+    
+    
+
+
     if(isset($_POST["submit"]))
     {
         if(empty($_POST["title"]))
@@ -19,21 +43,112 @@
         {
             $title = $_POST["title"];
         }
-    }
 
-    $type="";
-    $type_error ="";
-    $error = false;
 
-    if(empty($_POST["type"]))
-    {
-        $error = true;
-        $type_error="please select one";
-    }
-    else{
-        $type = $_POST["type"];
-    }
+// RATE VALIDATION
+        if(empty($_POST["rate"]))
+        {
+            $error = true;
+            $rate_error="This field is empty";
 
+        }
+        elseif(is_numeric($_POST["rate"]))
+        {
+            $rate = $_POST["rate"];
+
+        }
+
+        else
+        {
+            $error = true;
+            $rate_error = "only numeric allow !!";
+        }
+ 
+    
+
+ 
+// APARTMENT TYPE VALIDATION
+        if(!isset($_POST["type"]))
+        {
+            $error = true;
+            $type_error="please select one";
+        } else {
+            $type = $_POST["type"];
+        }
+
+        // BEDROOM VALIDATION
+        if(!isset($_POST["broom"]))
+        {
+            $error = true;
+            $room_error = "Please Select one..";
+        }
+
+        else
+        {
+            $broom = $_POST["broom"];
+        }
+
+// Area Validation
+        if(empty($_POST["area"]))
+        {
+            $error = true;
+            $area_error ="This Field is Empty";
+        }
+
+        elseif(!is_numeric($_POST["area"]))
+        {
+            $error = true;
+            $area_error = "Only numeric Allow";
+        }
+
+        else
+        {
+            $area = $_POST["area"];   
+        }
+    
+
+        // LOCATION VALIDATION
+
+        if(!isset($_POST["location"]))
+        {
+            $error = true;
+            $location_error="please select one";
+        } else {
+            $location = $_POST["location"];
+        }
+
+      
+
+//Adress validation
+        if(empty($_POST["adress"]))
+        {
+            $error = true;
+            $adress_error ="This Field is Empty";
+        }
+
+        elseif(is_numeric($_POST["adress"]))
+        {
+            $error = true;
+            $adress_error = "numeric is not Allow";
+        }
+
+        else
+        {
+            $adress = $_POST["adress"];   
+        }
+    
+      
+        if(!$error) {
+            echo "Post Title:" . $title . "<br>";
+            echo "Apartment Type :" .$type . "<br>";
+            echo "Apartment Rate :" .$rate . "<br>";
+            echo "Bed Room :" .$broom . "<br>";
+            echo "Area(SqFt) :" .$area . "<br>";
+            echo "Location :" .$location . "<br>";
+            echo "Adress :" .$adress;
+
+        }
+    }
 
 
 
@@ -79,13 +194,14 @@
             <td>Apartment Type</td>
             <td>
                 <select name="type">
-                    <option value ="" >Select</option>
+                    <option value ="" selected disabled>Select</option>
                     <?php
                         echo typePrint();
                     ?>
-                    <?php echo "$type_error" ?>
+                    
                     
                 </select>
+                <?php echo "$type_error" ?>
                 
                 
                
@@ -95,7 +211,9 @@
 
         <tr>
             <td>Rate</td>
-            <td><input type="text"></td>
+            <td><input type="text" name ="rate">
+            <?php echo "$rate_error" ?>
+            </td>
         </tr>
 
         <tr>
@@ -103,19 +221,23 @@
             <td>BedRoom</td>
           
             <td>
-                <select name="" id="">
-                <option value="">Select</option>
+                <select name="broom">
+                <option value=""selected disabled >Select</option>
 
                 <?php
                    echo numberPrint();
                 ?> 
                 </select>
+                <?php  echo "$room_error"; ?>
             </td>
         </tr>
 
         <tr>
-            <td>Area</td>
-            <td><input type="text"name="area"></td>
+            <td>Area(Ft)</td>
+            <td>
+            <input type="text"name="area">
+            <?php echo "$area_error"; ?>
+            </td>
         </tr>
 
         
@@ -124,18 +246,21 @@
             <td>Location</td>
             <td>
                 <select name="location">
-                    <option value="">Select</option>
+                    <option value="" selected disabled>Select</option>
                     <option value="location">Kuril</option>
                     <option value="location">Bashundhara</option>
                     <option value="location">Mirpur</option>
                     <option value="location">Gazipur</option>
-                </select>
+                </select> <?php echo "$location_error"; ?>
             </td>
         </tr>
 
         <tr>
             <td>Adress</td>
-            <td><textarea name="adress"></textarea></td>
+            <td>
+            <textarea name="adress"></textarea>
+            <?php  echo "$adress_error"; ?>
+            </td>
         </tr>
 
         
--- WT_MID/Project form/instrument_post.php
+++ WT_MID/Project form/instrument_post.php
@@ -1,5 +1,130 @@
 <?php
 
+$title="";
+$title_error="";
+$error = false;
+
+
+$type="";
+$type_error ="";
+
+
+$rate ="";
+$rate_error="";
+
+
+$condition="";
+$condition_error="";
+
+$location="";
+$location_error="";
+
+$adress="";
+$adress_error="";
+
+
+
+
+if(isset($_POST["submit"]))
+{
+    if(empty($_POST["title"]))
+    {
+        $error =true;
+        $title_error="This Field is Empty";
+    }
+
+    elseif(is_numeric($_POST["title"]))
+    {
+        $error = true;
+        $title_error = "Numeric is not allow ";
+    }
+    else
+    {
+        $title = $_POST["title"];
+    }
+
+    // Instrument TYPE VALIDATION
+    if(!isset($_POST["type"]))
+    {
+        $error = true;
+        $type_error="please select one";
+    } else {
+        $type = $_POST["type"];
+    }
+
+
+    // RATE VALIDATION
+    if(empty($_POST["rate"]))
+    {
+        $error = true;
+        $rate_error="This field is empty";
+
+    }
+    elseif(is_numeric($_POST["rate"]))
+    {
+        $rate = $_POST["rate"];
+
+    }
+
+    else
+    {
+        $error = true;
+        $rate_error = "only numeric allow !!";
+    }
+
+     // condition VALIDATION
+     if(!isset($_POST["condition"]))
+     {
+         $error = true;
+         $condition_error = "Please Select one condition..";
+     }
+
+     else
+     {
+         $condition = $_POST["condition"];
+     }
+
+// LOCATION VALIDATION
+
+        if(!isset($_POST["location"]))
+        {
+            $error = true;
+            $location_error="please select one";
+        } else {
+            $location = $_POST["location"];
+        }
+
+
+        //Adress validation
+        if(empty($_POST["adress"]))
+        {
+            $error = true;
+            $adress_error ="This Field is Empty";
+        }
+
+        elseif(is_numeric($_POST["adress"]))
+        {
+            $error = true;
+            $adress_error = "numeric is not Allow";
+        }
+
+        else
+        {
+            $adress = $_POST["adress"];   
+        }
+
+        if(!$error) {
+            echo "Post Title:" . $title . "<br>";
+            echo "Instrument Type :" .$type . "<br>";
+            echo " Rate :" .$rate . "<br>";
+            echo "Physical Condition :" .$condition . "<br>";
+            echo "Location :" .$location . "<br>";
+            echo "Adress :" .$adress ;
+
+        }
+
+}
+
 
 
 function instrumentPrint()
@@ -35,7 +160,10 @@ function locationPrint()
 
         <tr>
             <td>Post Title </td>
-            <td><input type="text"name="title"></td>
+            <td>
+                <input type="text"name="title">
+                <?php echo "$title_error"; ?>
+             </td>
         </tr>
 
         <tr>
@@ -49,11 +177,12 @@ function locationPrint()
                     <option value=""></option>
                 </select> -->
                 <select name="type">
-                    <option value="">Select</option> 
+                    <option value="" selected disabled>Select</option> 
                     <?php
                         instrumentPrint();
                    ?>
                 </select>
+                <?php echo "$type_error"; ?>
                 
                
                 
@@ -62,7 +191,10 @@ function locationPrint()
 
         <tr>
             <td>Rate</td>
-            <td><input type="text"></td>
+            <td>
+                <input type="text" name ="rate">
+                <?php echo "$rate_error" ;?>
+            </td>
         </tr>
 
         <tr>
@@ -70,10 +202,10 @@ function locationPrint()
             <td>Pysical Condition</td>
             <td>
                 
-                    <input type="radio"name="condition">Good
-                    <input type="radio"name="condition">Very Good
-                    <input type="radio"name="condition">Excellent
-                
+                    <input type="radio" name="condition">Good
+                    <input type="radio" name="condition">Very Good
+                    <input type="radio" name="condition">Excellent
+                    <?php echo "$condition_error"; ?>
             </td>
         </tr>
 
@@ -91,17 +223,20 @@ function locationPrint()
             </td> -->
             <td>
                 <select name="location">
-                    <option value="location">Select</option>
+                    <option value="location"selected disabled>Select</option>
                     <?php 
                         locationPrint();
                     ?>
-                </select>
+                </select> <?php echo "$location_error"; ?>
             </td>
         </tr>
 
         <tr>
             <td>Adress</td>
-            <td><textarea name="adress"></textarea></td>
+            <td>
+                <textarea name="adress"></textarea>
+                <?php echo "$adress_error"; ?>
+            </td>
         </tr>
 
         
